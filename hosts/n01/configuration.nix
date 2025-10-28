64effe24433b954c4f29915afaaa07f8ace9a628
Shen Chen <gordonchen2014@gmail.com>
2025-10-28 17:43:37 -0400

    rewrite nixos configs and remove homemanager

--- hosts/n01/configuration.nix
+++ hosts/n01/configuration.nix
@@ -3,52 +3,13 @@
   pkgs,
   ...
 }: {
-
   imports = [
     ./hardware-configuration.nix
-    ../../modules/nixos/desktop/niri
-    ../../modules/nixos/desktop/fonts
   ];
 
   nixpkgs.overlays = [
-    inputs.neovim-nightly-overlay.overlays.default
     inputs.niri-flake.overlays.niri
   ];
-
-  home-manager =
-    let
-      baseConfig = user: {
-        home = {
-          username = user;
-          homeDirectory = "/home/${user}";
-          stateVersion = "24.05";
-        };
-        programs.home-manager.enable = true;
-      };
-    in
-    {
-      useGlobalPkgs = true;
-      useUserPackages = true;
-
-      users."gordo" = {
-        imports = [
-          (baseConfig "gordo")
-          inputs.niri-flake.homeModules.niri
-
-          ../../modules/home-manager/apps/foot
-          ../../modules/home-manager/apps/qutebrowser
-
-          ../../modules/home-manager/cli/neovim
-          ../../modules/home-manager/cli/owen
-          ../../modules/home-manager/cli/git
-
-          ../../modules/home-manager/desktop/xdg
-          ../../modules/home-manager/desktop/gtk
-          ../../modules/home-manager/desktop/fuzzel
-          ../../modules/home-manager/desktop/niri
-        ];
-      };
-    };
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -61,6 +22,8 @@
 
   time.timeZone = "America/Toronto";
   i18n.defaultLocale = "en_CA.UTF-8";
+
+  time.hardwareClockInLocalTime = true;
 
   services.xserver.xkb = {
     layout = "us";
@@ -81,7 +44,7 @@
   };
 
   security.rtkit.enable = true;
-  
+
   services.pipewire = {
     enable = true;
     alsa.enable = true;
@@ -89,6 +52,8 @@
     pulse.enable = true;
     wireplumber.enable = true;
   };
+
+  services.flatpak.enable = true;
 
   virtualisation.docker.enable = true;
   services.dockerRegistry.enable = true;
@@ -100,12 +65,36 @@
       vim
       wget
       git
-      docker-compose
+      wl-clipboard
+      epiphany
+      nautilus
+      gnome-text-editor
+      gnome-disk-utility
     ];
     sessionVariables = {
       WLR_NO_HARDWARE_CURSORS = "1";
     };
   };
+
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      roboto
+      adwaita-fonts
+      cherry
+    ];
+  };
+
+  programs.niri = {
+    enable = true;
+    package = pkgs.niri-unstable;
+  };
+
+  environment.variables.NIXOS_OZONE_WL = "1";
+
+  programs.dconf.enable = true;
 
   system.stateVersion = "24.05";
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
